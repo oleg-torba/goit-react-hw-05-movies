cc6b2c47d399025aeffb7161af9db7fe9a04103a
oleg-torba <100319055+oleg-torba@users.noreply.github.com>
2023-05-09 08:21:41 +0300

    some

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,7 +1,7 @@
-import { Link } from "react-router-dom"
+
 
 const MovieList = ({movie}) => {
-   const {title, vote_average, overview, genres, poster_path} = movie
+   const {title, vote_average, overview, poster_path} = movie
    return (
       <><div>
          <img src={'https://image.tmdb.org/t/p/w500' + poster_path} alt=""/>
